65cccc5e578842293234c09db48bf8c78c64ffdd
diogo vieira <diogo.vcastro25@gmail.com>
2025-01-25 15:43:54 -0300

    creating and defining Pessoa class

---
+++ src/Pessoa.java
@@ -0,0 +1,35 @@
+public abstract class Pessoa {
+    private String CPF;
+    private String nome;
+    private String dataNascimento;
+
+    public Pessoa(String CPF, String nome, String dataNascimento) {
+        this.CPF = CPF;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
