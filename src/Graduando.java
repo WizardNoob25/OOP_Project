17791c22251166d48a0dfa920b78cc20711cc6a6
diogo vieira <diogo.vcastro25@gmail.com>
2025-01-25 15:46:19 -0300

    creating and defining Graduando class

---
+++ src/Graduando.java
@@ -0,0 +1,16 @@
+public class Graduando extends Estudante{
+    private Professor supervisor;
+
+    public Graduando(String CPF, String nome, String dataNascimento, Double CRA, Professor supervisor) {
+        super(CPF, nome, dataNascimento, CRA);
+        this.supervisor = supervisor;
+    }
+
+    public Professor getSupervisor() {
+        return supervisor;
+    }
+
+    public void setSupervisor(Professor supervisor) {
+        this.supervisor = supervisor;
+    }
+}
