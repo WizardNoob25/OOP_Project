8e721b7d2411b7a3c66441d67a9e58fa3be36e10
diogo vieira <diogo.vcastro25@gmail.com>
2025-01-25 15:46:49 -0300

    creating and defining Pos_Grad class

---
+++ src/Pos_Grad.java
@@ -0,0 +1,16 @@
+public class Pos_Grad extends Estudante{
+    private String temaPesquisa;
+
+    public Pos_Grad(String CPF, String nome, String dataNascimento, Double CRA, String temaPesquisa) {
+        super(CPF, nome, dataNascimento, CRA);
+        this.temaPesquisa = temaPesquisa;
+    }
+
+    public String getTemaPesquisa() {
+        return temaPesquisa;
+    }
+
+    public void setTemaPesquisa(String temaPesquisa) {
+        this.temaPesquisa = temaPesquisa;
+    }
+}
