eb67be3e49c6db2b62a6568f28f421062df1220d
diogo vieira <diogo.vcastro25@gmail.com>
2025-01-26 00:56:54 -0300

    creating and defining Disciplina class

---
+++ src/Disciplina.java
@@ -0,0 +1,35 @@
+public class Disciplina {
+    private String codigo;
+    private String nome;
+    private Integer cargaHoraria;
+
+    public Disciplina(String codigo, String nome, Integer cargaHoraria) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(Integer cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+}
